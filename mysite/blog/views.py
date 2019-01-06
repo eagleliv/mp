ba92b2674af2721e18792cb6277909a212eabb22
eagleliv <lyapin17@gmail.com>
2019-01-06 19:17:32 +0200

    11

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -16,13 +16,12 @@ def blog_details(request, pk):
 class PostCreate(View):
     def get(self, request):
         form = PostForm()
-        print(form)
-        form.author = request.user.username()
-
         return render(request, 'blog/post_create.html', context={'form': form})
     def post(self, request):
         bound_form = PostForm(request.POST)
         if bound_form.is_valid():
+            new_post = bound_form.save(commit = False)
+            new_post.author = request.user.username
             new_post = bound_form.save()
             return redirect('blog_details', pk=new_post.id)
         return render(request, 'blog/post_create.html', context={'form': bound_form})
