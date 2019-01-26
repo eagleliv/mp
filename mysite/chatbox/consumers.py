f8c0fb7f441d8182ff81eac3fbb9ed5657645345
eagleliv <lyapin17@gmail.com>
2019-01-26 17:40:08 +0200

    Merge branch 'master' of https://github.com/eagleliv/mp

--- mysite/chatbox/consumers.py
+++ mysite/chatbox/consumers.py
@@ -1,49 +1,55 @@
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+import asyncio
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+from channels.layers import get_channel_layer
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connection_groups(self):
+        print("iii")
+    async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
-        self.user_name = self.scope['user']
+        self.room_names = []
+        self.room_names.append(self.room_name)
+        print(self.scope['user'])
+        # Join romm group
+        await self.channel_layer.group_add(
+            self.room_name,
+            self.channel_name
+        )
+        await self.accept()
 
-        print(self.user_name)
-
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
+    async def disconnect(self, close_code):
+        self.room_names.remove(self.room_name)
+        print(self.room_names)
+        await self.channel_layer.group_discard(
+            self.room_name,
             self.channel_name
         )
 
-        self.accept()
-
-    def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
-            self.channel_name
-        )
-
-    # Receive message from WebSocket
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        # Receive message from WebSocket
+    async def receive(self, text_data):
+        handled_text = json.loads(text_data)
+        chat_text = handled_text['chat_text']
+        user = handled_text['user']
+        message = {
+            'message': chat_text,
+            'user': user
+        }
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
+        await self.channel_layer.group_send(
+            self.room_name,
             {
-                'type': 'chat_message',
-                'message': message,
+                'type': 'show_text',
+                'text': message
             }
         )
 
     # Receive message from room group
-    def chat_message(self, event):
-        message = event['message']
+    async def show_text(self, event):
+        message = event['text']
 
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        #Send message to WebSocket
+        await self.send(text_data = json.dumps({
             'message': message
         }))
